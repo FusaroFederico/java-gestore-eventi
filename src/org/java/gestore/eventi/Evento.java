6db76b848b5c9397fd8ea5c390a2bc178cddfd1f
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-07-02 15:32:29 +0200

    Add instance variables and constructor method to Evento.java

--- src/org/java/gestore/eventi/Evento.java
+++ src/org/java/gestore/eventi/Evento.java
@@ -1,5 +1,19 @@
 package org.java.gestore.eventi;
 
-public class Evento {
+import java.util.GregorianCalendar;
 
+public class Evento {
+	// variabili di istanza
+	private String titolo;
+	private GregorianCalendar data;
+	private int postiTotale;
+	private int postiPrenotati;
+	
+	// costruttore
+	public Evento(String titolo, GregorianCalendar data, int postiTotale) {
+		this.titolo = titolo;
+		this.data = data;
+		this.postiTotale = postiTotale;
+		this.postiPrenotati = 0;
+	}
 }
