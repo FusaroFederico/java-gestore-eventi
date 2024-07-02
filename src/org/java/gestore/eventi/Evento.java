cc7adb9eda7fa3d84a57c8b89f964b9c64c92cfc
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-07-02 17:06:17 +0200

    Add getters and setters to Evento.java

--- src/org/java/gestore/eventi/Evento.java
+++ src/org/java/gestore/eventi/Evento.java
@@ -18,4 +18,38 @@ public class Evento {
 		this.postiTotale = postiTotale;
 		this.postiPrenotati = 0;
 	}
+
+	// Getters and Setters
+	public String getTitolo() {
+		return this.titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public GregorianCalendar getData() {
+		return this.data;
+	}
+
+	public void setData(GregorianCalendar data) {
+		this.data = data;
+	}
+
+	public int getPostiTotale() {
+		return this.postiTotale;
+	}
+
+	private void setPostiTotale(int postiTotale) {
+		this.postiTotale = postiTotale;
+	}
+
+	public int getPostiPrenotati() {
+		return this.postiPrenotati;
+	}
+
+	private void setPostiPrenotati(int postiPrenotati) {
+		this.postiPrenotati = postiPrenotati;
+	}
+	
 }
