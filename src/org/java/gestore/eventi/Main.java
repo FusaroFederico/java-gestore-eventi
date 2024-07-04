8018d0321a2a733b1a325095ae7312490e9aedda
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-07-04 16:59:47 +0200

    Update Main.java
    Main.java ask user if wants to create a new Evento

--- src/org/java/gestore/eventi/Main.java
+++ src/org/java/gestore/eventi/Main.java
@@ -8,6 +8,21 @@ public class Main {
 	public static void main(String[] args) {
 		// istanza di scanner con input da terminale
 		Scanner scanner = new Scanner(System.in);
+		
+		// chiede all'utente se vuole inserire un nuovo evento
+		System.out.println("Vuoi inserire un nuovo evento si/no?");
+		String sceltaUtente = scanner.nextLine();
+		
+		// switch
+		switch (sceltaUtente) {
+		case "si":      // chiede all'utente di inserire degli input per istanziare un nuovo evento
+			break;
+		case "no":
+			System.out.println("Arrivederci.");
+			break;
+		default:
+			System.out.println("Hai inserito un input non corretto.");
+		}
 		scanner.close();
 	}
 
