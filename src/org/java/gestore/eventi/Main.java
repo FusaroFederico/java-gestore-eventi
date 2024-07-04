805a8e7050b7bf4b327511ff4c115b9eb29cb50c
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-07-04 17:05:53 +0200

    Update Main.java
    Now Main.java ask user to insert parameters required to Evento constructor and instance a new Evento evento.

--- src/org/java/gestore/eventi/Main.java
+++ src/org/java/gestore/eventi/Main.java
@@ -16,6 +16,23 @@ public class Main {
 		// switch
 		switch (sceltaUtente) {
 		case "si":      // chiede all'utente di inserire degli input per istanziare un nuovo evento
+			// input utente: titolo
+			System.out.println("Inserisci il titolo dell'evento:");
+			String titolo = scanner.nextLine();
+			
+			// input utente: data
+			System.out.println("Inserisci la data in cui avverrà l'evento nel formato gg/mm/aaaa :");
+			String dataUtente = scanner.nextLine();
+			// istanza di GregorianCalendar data
+			int[] dataArrayInt = EventoUtils.dataToIntArray(dataUtente);
+			GregorianCalendar data = new GregorianCalendar(dataArrayInt[2], dataArrayInt[1] - 1, dataArrayInt[0]);
+			
+			// input utente: posti
+			System.out.println("Quanti posti saranno disponibili per l'evento?");
+			String posti = scanner.nextLine();
+			
+			// crea nuovo evento
+			Evento evento = new Evento(titolo, data, Integer.parseInt(posti));
 			break;
 		case "no":
 			System.out.println("Arrivederci.");
