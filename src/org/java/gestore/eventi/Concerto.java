d0a9b6d1dd757a767c8025d77c5334ccea734e7a
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-07-04 17:22:10 +0200

    Create Concerto.java
    Concerto.java extends Evento.
    Are implemented constructor, getters and setters

---
+++ src/org/java/gestore/eventi/Concerto.java
@@ -0,0 +1,36 @@
+package org.java.gestore.eventi;
+
+import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Concerto extends Evento {
+	// attributi
+	private LocalTime ora;
+	private double prezzo;
+	
+	// costruttore
+	public Concerto(String titolo, GregorianCalendar data, int postiTotale, LocalTime ora, double prezzo) {
+		super(titolo, data, postiTotale);
+		this.ora = ora;
+		this.prezzo = prezzo;
+	}
+	
+	// getters and setters
+	public LocalTime getOra() {
+		return ora;
+	}
+
+	public void setOra(LocalTime ora) {
+		this.ora = ora;
+	}
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+	
+}
