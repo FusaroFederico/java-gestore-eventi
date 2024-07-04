40cf2a9fb51d203ce65e5da80cb025990ef55ade
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-07-04 17:16:02 +0200

    Update Main.java
    Now Main.java asks the user if and how many seats he wants to book.
    Input is checked and then the seats requested by the user are booked.
    Lastly print number of seats booked and seats still available.

--- src/org/java/gestore/eventi/Main.java
+++ src/org/java/gestore/eventi/Main.java
@@ -33,6 +33,43 @@ public class Main {
 			
 			// crea nuovo evento
 			Evento evento = new Evento(titolo, data, Integer.parseInt(posti));
+				
+			// chiede all'utente se vuole effettuare prenotazioni
+			System.out.println("Vuoi effettuare prenotazioni si/no?");
+			String sceltaPrenota = scanner.nextLine();
+			
+			// se la risposta è si: chiede all'utente quanti posti vuole prenotare
+			if (sceltaPrenota.equals("si")) {
+				boolean checkInputUtente = false; // variabile boolean di controllo dell'input
+				int postiDaPrenotare = 0;
+				
+				// ciclo while per il controllo dell'input postiDaPrenotare
+				while (!checkInputUtente) {
+					checkInputUtente = true;
+					System.out.println("Quanti posti vuoi prenotare per l'evento?");
+					postiDaPrenotare = Integer.parseInt(scanner.nextLine());
+					// se inserisce un numero negativo dà un messaggio di errore e chiede
+					// nuovamente all'utente quanti posti vuole prenotare
+					if (postiDaPrenotare < 0) {
+						System.out.println("Attenzione! Hai inserito un numero negativo. Riprova.");
+						checkInputUtente = false;
+					}
+					// se l'input utente eccede il numero di posti totali dà un messaggio
+					// di errore e chiede nuovamente di inserire l'input
+					if (postiDaPrenotare > evento.getPostiTotale()) {
+						System.out.println("Attenzione! L'evento ha al massimo " + evento.getPostiTotale() + " posti prenotabili.");
+						checkInputUtente = false;
+					}
+				}
+				// ciclo for che richiama ad ogni iterazione prenota() postiDaPrenotare volte
+				for (int i = 1; i <= postiDaPrenotare; i++) {
+					evento.prenota();
+				}
+			}
+			// stampa i posti prenotati 
+			System.out.println("I posti prenotati sono: " + evento.getPostiPrenotati() + ".");
+			// stampa i posti disponibili
+			System.out.println("Sono ancora disponibili: " + (evento.getPostiTotale() - evento.getPostiPrenotati()) + " posti.");
 			break;
 		case "no":
 			System.out.println("Arrivederci.");
