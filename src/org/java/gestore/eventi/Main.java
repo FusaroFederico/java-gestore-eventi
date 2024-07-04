4a53dd80497d7a49b66fcb7f71365599a407042b
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-07-04 16:55:55 +0200

    Create Main.java

---
+++ src/org/java/gestore/eventi/Main.java
@@ -0,0 +1,14 @@
+package org.java.gestore.eventi;
+
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// istanza di scanner con input da terminale
+		Scanner scanner = new Scanner(System.in);
+		scanner.close();
+	}
+
+}
