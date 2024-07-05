fc9c97dd190bff5d24f1ee3bedf073839f5a796e
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-07-05 16:21:07 +0200

    Create ProgrammaEventi.java

---
+++ src/org/java/gestore/eventi/ProgrammaEventi.java
@@ -0,0 +1,35 @@
+package org.java.gestore.eventi;
+
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+public class ProgrammaEventi {
+	// attributi
+	private String titolo;
+	private List<Evento> eventi;
+	
+	// costruttore
+	public ProgrammaEventi(String titolo) {
+		this.titolo = titolo;
+		this.eventi = new ArrayList<Evento>();
+	}
+	
+	// getters and setters
+	public String getTitolo() {
+		return titolo;
+	}
+
+	private void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public List<Evento> getEventi() {
+		return eventi;
+	}
+
+	private void setEventi(List<Evento> eventi) {
+		this.eventi = eventi;
+	}
+	
+}
